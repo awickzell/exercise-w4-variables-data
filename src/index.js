431ed5d6f80070df00633970d6dd8a06d4b8f716
Arvid Wickzell <arvidwickzell345@hotmail.com>
2024-10-18 11:37:19 +0200

    add js code

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
 Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
-const cafeName = "Technigo Cafe";
-console.log(`Welcome to ${cafeName}! What would you like to order today?`);
+const cafeName = "Café Java"
+
+console.log('Welcome to ${cafeName}! What would you like to order today');
 
 /*
 2)
@@ -14,6 +15,11 @@ Create a variable that stores how many coffees the customer wants.
 Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
+let coffeePrice = 5; // pris per kaffe
+let coffeeAmount = 2; // antal kaffe kunden vill ha
+let totalPrice = coffeePrice * coffeeAmount;
+
+console.log(`There you go, that'll be ${totalPrice} euros`);
 
 /*
 3)
@@ -21,6 +27,8 @@ Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
+let isBestCoffee = true; // boolean variabel
+console.log(`You said this coffee is the best, that was actually ${isBestCoffee}`);
 
 /*
 4)
@@ -30,6 +38,39 @@ Print it out.
 Assign it a new value.
 Print it out. => This should give you the new value.
 */
+let cafeGuests = 10; // antal gäster just nu
+console.log(`We currently have ${cafeGuests} guests in the cafe`);
+
+cafeGuests = 15; // ändra antal gäster
+console.log(`Now we have ${cafeGuests} guests in the cafe`);
+
+
+
+/*
+6)
+Create a variable that stores a string.
+Print out that string in only UPPERCASE letters.
+*/
+let cafeLatte = 'Cafe Latte is really good'; // strängvariabel
+console.log(cafeLatte.toUpperCase()); // UPPERCASE
+
+/*
+7)
+Print out the same string in only lowercase letters.
+*/
+console.log(cafeLatte.toLowerCase()); // lowercase
+
+/*
+8) **BONUS**
+Print out the string "Today we have a special summer deal!". 
+Then figure out a way to replace the word "summer" in the string with the word "winter"
+Should give you => "Today we have a special winter deal!"
+(check for a specific string method...)
+*/
+let specialDeal = "Today we have a special summer deal!";
+let updatedDeal = specialDeal.replace("summer", "winter");
+
+console.log(updatedDeal); // "Today we have a special winter deal!
 
 /*
 5)
@@ -39,22 +80,9 @@ Print it out.
 assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
+const maxGuests = 50; // max antal gäster tillåtet
+console.log(`The maximum number of guests allowed is ${maxGuests}`);
 
-/*
-6)
-Create a variable that stores a string.
-Print out that string in only UPPERCASE letters.
-*/
-
-/*
-7)
-Print out the same string in only lowercase letters.
-*/
-
-/*
-8) **BONUS**
-Print out the string "Today we have a special summer deal!". 
-Then figure out a way to replace the word "summer" in the string with the word "winter"
-Should give you => "Today we have a special winter deal!"
-(check for a specific string method...)
-*/
+// Försöker vi ändra värdet på en konstant ger det ett fel:
+maxGuests = 60; // This should give you an error because it shouldn't be able to be changed.
+console.log(`New max guests: ${maxGuests}`); // detta kommer inte att köras
